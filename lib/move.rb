3144bd6aa18046c46d5119d23b96ebd95d0091c1
Gabriel Aguilar <gabe.p.aguilar@gmail.com>
2020-08-29 05:47:44 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,12 +6,3 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-
-
-def input_to_index(input)
-  board[input.to_i - 1]
-end
-
-def move(board, index, value)
-  board[index] = value
-end
